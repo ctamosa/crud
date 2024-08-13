2fd107c7b6fe0f67e83bd1972330e3819c5eeb48
Tamosa Chakraborty <ctamo.net@gmail.com>
2024-08-13 11:59:26 +0530

    test

---
+++ db.js
@@ -0,0 +1,21 @@
+// db.js
+const mysql = require("mysql2");
+
+// Database connection details
+const db = mysql.createConnection({
+  host: "database-1.cvgio2aq09cd.ap-south-1.rds.amazonaws.com",
+  user: "admin",
+  password: "Tamosa1985",
+  port: "3306",
+  database: "tamosa",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err.stack);
+    return;
+  }
+  console.log("Connected to the database.");
+});
+
+module.exports = db;
